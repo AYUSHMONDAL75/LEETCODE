9b92cfb3ece093d77f6c34a61e07b2cc4849ab40
AYUSH MONDAL <146535405+AYUSHMONDAL75@users.noreply.github.com>
2026-08-03 01:42:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (48.61%) - LeetSync

---
+++ 50-powx-n/powx-n.c
@@ -0,0 +1,20 @@
+double myPow(double x, int n) {
+    if(n==0){
+        return 1;
+    }
+    long binary=n;
+    if(n < 0){
+        x=1/x;
+        binary=-binary;
+    }
+    double ans = 1;
+    while(binary> 0){
+        if(binary % 2 ==1){
+            ans *= x;
+        }
+        x *= x;
+        binary /= 2;
+    }
+    return ans;
+
+}
