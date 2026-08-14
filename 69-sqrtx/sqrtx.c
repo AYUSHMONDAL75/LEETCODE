10f360054e2c2d6f326af9908605ef36c5a96aff
AYUSH MONDAL <146535405+AYUSHMONDAL75@users.noreply.github.com>
2026-08-14 09:19:59 +0530

    sqrtx.c

--- 69-sqrtx/sqrtx.c
+++ 69-sqrtx/sqrtx.c
@@ -6,7 +6,7 @@ int mySqrt(int x) {
     int left = 1, right = x, ans = 0;
     
     while (left <= right) {
-        int mid = left + (right - left) / 2;
+        int mid = (right + left) / 2;
         
         if (mid <= x / mid) {
             ans = mid;
