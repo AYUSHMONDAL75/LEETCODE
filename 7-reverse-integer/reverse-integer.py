3552f31918afe90cbfa5863d1b8c2bc4747964bf
AYUSH MONDAL <146535405+AYUSHMONDAL75@users.noreply.github.com>
2026-09-22 22:24:11 +0530

    Time: 13 ms (82.10%) | Memory: 12.4 MB (52.65%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+           res = -int(str(abs(x))[::-1])
+        else:
+            res = int(str(x)[::-1])
+
+        if res < -2**31 or res > 2**31 - 1:
+            return 0 
+        return res
