0a41413562f9ecd74770d19cdf8c3025ca11aa6c
AYUSH MONDAL <146535405+AYUSHMONDAL75@users.noreply.github.com>
2026-09-22 22:26:11 +0530

    Time: 21 ms (32.33%) | Memory: 12.3 MB (88.21%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x < 0:
+        if (x < 0):
             res = -int(str(abs(x))[::-1])
         else:
             res = int(str(x)[::-1])
-            
-        if res < -2**31 or res > 2**31 - 1:
+
+        if (res < -2**31 or res > 2**31 - 1):
             return 0
 
         return res
